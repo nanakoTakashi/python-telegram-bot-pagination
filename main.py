9f90d103c042886151a10cbbbbdccc9d51532988
nnk0o <91994223+nanakoTakashi@users.noreply.github.com>
2023-01-27 12:39:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import telebot
+c = telebot.TeleBot("5639296247:AAHYQdeXMkM-cHOybxmA2bBriuD6FOy6Vgk")
+@c.message_handler(commands=['start'])
+def start_c(m):
+    c.send_message(m.chat.id,m.text)
 def main():
-  print("Hello, World!")
+    c.polling()
 if __name__=="__main__":
   main()
